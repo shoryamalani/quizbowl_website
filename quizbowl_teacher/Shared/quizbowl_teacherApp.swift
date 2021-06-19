ff9f44191bd7c77e04e5c7844e65466f8c226b31
Aryaman Lahoti <aryamanlahoti@Aryamans-Air.attlocal.net>
2021-06-19 15:22:06 -0500

    making project

---
+++ quizbowl_teacher/Shared/quizbowl_teacherApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizbowl_teacherApp.swift
+//  Shared
+//
+//  Created by Aryaman Lahoti on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct quizbowl_teacherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
