2764d7aa430bd604a915c9fc411eb2c48fabd323
Shorya Malani <shoryamal@gmail.com>
2021-06-22 08:41:37 -0400

    testing xcode on my computer

--- quizbowl_teacher/Shared/ContentView.swift
+++ quizbowl_teacher/Shared/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("This is a test").padding()
+            }.padding()
+        }
+        
     }
 }
 
