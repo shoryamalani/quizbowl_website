2fc4f4a406d92af07c13dc68e4d4b7d02a19c217
Shorya Malani <shoryamal@gmail.com>
2021-06-24 17:50:39 -0400

    adding a list to our app

--- quizbowl_teacher/Shared/ContentView.swift
+++ quizbowl_teacher/Shared/ContentView.swift
@@ -15,6 +15,12 @@ struct ContentView: View {
             Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
                 Text("This is a test").padding()
             }.padding()
+            List {
+                Text("This is a list")
+                Text("This is a list 1")
+                Text("This is a list 2")
+            }
+
         }
         
     }
