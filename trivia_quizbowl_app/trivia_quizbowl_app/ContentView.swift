e1d2cea04fd2938c9377c39081733b0a9ec88cec
Arnav Lahoti <lahoti500@gmail.com>
2021-07-18 15:47:15 -0500

    lol

--- trivia_quizbowl_app/trivia_quizbowl_app/ContentView.swift
+++ trivia_quizbowl_app/trivia_quizbowl_app/ContentView.swift
@@ -206,7 +206,7 @@ struct ContentView: View {
             RadialGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)), Color(#colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1))]), center: .center, startRadius: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/, endRadius: /*@START_MENU_TOKEN@*/500/*@END_MENU_TOKEN@*/).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack() {
                 
-                Text("Points: \(String(points))").font(.headline).padding(.vertical, 20.0).padding(.horizontal).background(Color.yellow)
+                Text("Points: \(String(points))").font(.headline).padding(.vertical, 10.0).padding(.horizontal).background(Color.yellow)
                 Text("Stats for this session: \(totalQuestionsCorrect) questions correct and \(totalNegatives) incorrect attempts").padding().background(Color.orange)
                 VStack() {
                     //shows the correct answer with spelling and everything
@@ -217,7 +217,7 @@ struct ContentView: View {
                         .foregroundColor(Color.white)
                         .padding()
                         .background(Color(#colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)))
-                        .minimumScaleFactor(0.2)
+                        .minimumScaleFactor(0.6)
                     Text("This Question: \(correctThisQuestion)").font(.headline).padding(.horizontal).padding(.vertical, 20.0).background(tryAgainOrCorrect).opacity(0.8).onReceive(gameTimer, perform: { timer in
                         addWordAndCheckNeed()
                     }) // This is where it shows if the question is right NEEDS CHANGING
@@ -226,7 +226,7 @@ struct ContentView: View {
                 // On the other hand onCommit is when you hit enter
                 TextField("Write answer here", text: $answerFromUser,onCommit:{
                     submitAnswerAndGetNewQuestion()
-                }).padding().border(Color.gray, width: 2).cornerRadius(3.0).opacity(showAnswerBox ? 1 : 0)
+                }).padding().border(Color.gray, width: 2).cornerRadius(3.0).opacity(showAnswerBox ? 1 : 0).foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
                 VStack(){
                     Button(action: submitAnswerAndGetNewQuestion) {
                         Text(buzzOrSubmit).padding()
@@ -235,9 +235,9 @@ struct ContentView: View {
                     Button(action: nextQuestion) {
                         Text("Get New Question").padding()
                     } // gets a new question
-                    Button(action: skipToEnd){
-                        Text("Skip To The End").padding()
-                    }
+                 //   Button(action: skipToEnd){
+                   //     Text("Skip To The End").padding()
+                    // }
                 }.opacity(showInRoundMode ? 1 : 0) // This should be shown when in round
                 if showInRoundMode == false{
                     VStack(){
@@ -246,7 +246,6 @@ struct ContentView: View {
                         }
                     }
                 }
-                Spacer()
     //            Button(action: resetScore){
     //                Text("Reset Score").padding().padding(.bottom)
     //            } // Resets Score NEEDS CHANGING
