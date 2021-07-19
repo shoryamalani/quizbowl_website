5519e73b087e4302cac06eee2c4e9dcd764d3c4f
Shorya Malani <shoryamal@gmail.com>
2021-07-19 16:37:00 -0400

    testing certain difficulties

--- quizbowl_backend_server/app.py
+++ quizbowl_backend_server/app.py
@@ -34,7 +34,7 @@ def return_template_question_with_difficulty():
     for question in questions:
         print(question)
         question = question[0]
-        print(question[2][0])
+        print(question)
         final_questions.append({"question":question[2].replace("&apos;","'").split(),"questionId":question[0],"answer":question[4]})
     return jsonify(final_questions)
 @app.route("/get_round_questions")
