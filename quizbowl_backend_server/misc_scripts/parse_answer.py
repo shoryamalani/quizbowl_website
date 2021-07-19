4338d30c3fdfa6c033c97a71eb14c098e45cde80
Shorya Malani <shoryamal@gmail.com>
2021-07-18 21:09:44 -0400

    fixing moronic behaviour

--- quizbowl_backend_server/misc_scripts/parse_answer.py
+++ quizbowl_backend_server/misc_scripts/parse_answer.py
@@ -24,6 +24,7 @@ def check_answer_from_user(user_answer,correct_answer):
     correct_answer = correct_answer.replace("<em>", ' ')
     correct_answer = correct_answer.replace("</em>", ' ')
     distance = jellyfish.damerau_levenshtein_distance(user_answer, correct_answer)
+    print(distance)
     if distance>len(user_answer)*0.2:
         return [True, correct_answer]
     return [False, correct_answer]
