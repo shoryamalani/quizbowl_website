cc7559e253ba03973e1824ecdf125666665cd897
Arnav Lahoti <lahoti500@gmail.com>
2021-07-20 16:40:35 -0500

    i was trying out some stuff but it didnt work cuz i dont have theactual logo file on my computer

---
+++ trivia_quizbowl_app/trivia_quizbowl_app/Info Screen/Views/InfoView.swift
@@ -0,0 +1,24 @@
+//
+//  InfoView.swift
+//  trivia_quizbowl_app
+//
+//  Created by Arnav Lahoti on 7/19/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Text("hi")
+            }
+            .navigationTitle("Information")
+            }
+        }
+    }
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
