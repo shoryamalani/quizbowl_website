fd78eb45dc57d854c864ca88ac89d51dc614e360
Shorya Malani <shoryamal@gmail.com>
2022-01-04 19:28:33 -0500

    fixing move databases

--- quizbowl_backend_server/dbs_scripts/move_databases.py
+++ quizbowl_backend_server/dbs_scripts/move_databases.py
@@ -3,14 +3,14 @@
 from __future__ import print_function
 from typing import Tuple
 from create_database import create_table_command
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# import os
+# import sys
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import psycopg2
-from .write_and_read_to_database import *
-from .get_data_from_database import *
-from .execute_db import *
-from .create_database import *
+from write_and_read_to_database import *
+from get_data_from_database import *
+from execute_db import *
+from create_database import *
 from datetime import datetime
 def main():
     db_connection = connect_to_datbase("localhost","smalani","trivia_app_db")
