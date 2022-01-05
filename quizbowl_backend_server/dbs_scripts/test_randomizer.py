353b5197f00b614188f0cc60a8b625f5a20712a5
Shorya Malani <shoryamal@gmail.com>
2022-01-05 15:01:33 -0500

    adding a tester to the randomizer function

--- quizbowl_backend_server/dbs_scripts/test_randomizer.py
+++ quizbowl_backend_server/dbs_scripts/test_randomizer.py
@@ -7,10 +7,11 @@ db_connection = connect_to_datbase("localhost","smalani",current_db_g) # IO
 all_questions = {}
 while len(all_questions) < 356:
     id_data = execute_database_command(db_connection,"SELECT uuid FROM original_questions TABLESAMPLE SYSTEM(1) where difficulty=8 and topic=19 and status=1 limit 1;")
-    print(id_data[1].fetchone())
-    # id = int(id_data[1].fetchone()[0])
-    # if id in all_questions:
-    #     all_questions[id] += 1
-    # else:
-    #     all_questions[id] = 1
+    id = id_data[1].fetchone()
+    if id != None:
+        id = id[0]
+    if id in all_questions:
+        all_questions[id] += 1
+    else:
+        all_questions[id] = 1
 print(all_questions)
