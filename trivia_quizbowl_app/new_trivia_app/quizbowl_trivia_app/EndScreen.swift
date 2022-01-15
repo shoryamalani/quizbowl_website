9b49ac4d9504ba6ab8aa677cae14384b79b23526
Arnav Lahoti <lahoti500@gmail.com>
2022-01-15 17:42:52 -0600

    just added a few ideas. can't really do much without knowing what variables are used and where they're used

---
+++ trivia_quizbowl_app/new_trivia_app/quizbowl_trivia_app/EndScreen.swift
@@ -0,0 +1,20 @@
+//
+//  EndScreen.swift
+//  quizbowl_trivia_app
+//
+//  Created by Arnav Lahoti on 1/15/22.
+//
+
+import SwiftUI
+
+struct EndScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EndScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        EndScreen()
+    }
+}
