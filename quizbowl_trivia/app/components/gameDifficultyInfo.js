56ffd2c98541accb4e0938e47485901877d20df0
Arnav Lahoti <lahoti500@gmail.com>
2022-07-20 23:23:31 +0530

    more front end u know the dealio

--- quizbowl_trivia/app/components/gameDifficultyInfo.js
+++ quizbowl_trivia/app/components/gameDifficultyInfo.js
@@ -7,7 +7,7 @@ function gameDifficultyInfo(props) {
         <Modal visible={props.visible} animationType="slide">
         <SafeAreaView>
             <Pressable onPress={() => { console.log('henlo'); props.switchModals() }} >        
-                <Text> Henlo! </Text>    
+                <Text> Game difficulty is based off what levels certain questions are read to in tournaments. For example, a Level 5 round would consist of questions read in tournaments at the national level for high schoolers. If you find that you are consistently able to answer questions very early in the question, then try to challenge yourself by attempting a round with a higher difficulty! On the other hand, if you are doing a round where you do not know </Text>    
             </Pressable>
         </SafeAreaView>
         </Modal>
