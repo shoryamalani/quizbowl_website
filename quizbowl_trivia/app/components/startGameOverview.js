9c4e3f053046f15693f38b598cf095d9e76fafc9
Shorya Malani <shoryamal@gmail.com>
2022-07-20 21:52:46 +0530

    made game screen kinda work

--- quizbowl_trivia/app/components/startGameOverview.js
+++ quizbowl_trivia/app/components/startGameOverview.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { Modal, Button, View, StyleSheet, Alert } from 'react-native';
+import { Modal, Button,SafeAreaView,Text, View, StyleSheet, Alert,Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { Slider } from '@miblanchard/react-native-slider';
 
 function StartGameOverview(props) {
   function getQuestions(){
@@ -26,13 +27,19 @@ function StartGameOverview(props) {
         Alert.alert("Error", "Could not get questions");
     });
   }
-  return (<Modal visible={props.visible} animationType="slide">
+  return (
+    <>
     <StatusBar style="light" />
-      <View style={styles.inputContainer}>
-        <Button title='Start Game' onPress={getQuestions}/>
-      </View>
+    <Modal visible={props.visible} animationType="slide">
+    
+      <SafeAreaView style={styles.inputContainer}>
+        <Text>LETS TRY THIS</Text>
+        <Button title='Start Game' onPress={getQuestions}/> 
+         <Slider></Slider>
+      </SafeAreaView>
       
     </Modal>
+    </>
   )
 };
 
@@ -41,7 +48,7 @@ export default StartGameOverview;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
