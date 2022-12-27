581fc287fa0dd9a079096b21a9c7f691e16fdd9b
Shorya Malani <shoryamal@gmail.com>
2022-12-27 13:25:26 -0600

    app works again with redux enabled and is ready for persistant stuff

--- quizbowl_trivia/utils/store.js
+++ quizbowl_trivia/utils/store.js
@@ -1,12 +1,71 @@
-import { configureStore } from '@reduxjs/toolkit'
-import gameSlice from '../features/game/gameSlice'
-import { devToolsEnhancer } from 'redux-devtools-extension';
+// import { configureStore,getDefaultMiddleware } from '@reduxjs/toolkit'
+// import gameSlice from '../features/game/gameSlice'
+// import {
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist'
+// // import storage from 'redux-persist/lib/storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default configureStore({
-  reducer: {
-    game: gameSlice, 
-  },
+
+// const persistConfig = {
+//   key: 'root',
+//   version: 1,
+//   storage: AsyncStorage,
+// }
+
+// export const persistedReducer = persistReducer(persistConfig, {
+//   game: gameSlice,
+// })
+
+// const store =  configureStore({
+//   reducer: persistedReducer,
+//   middleware: getDefaultMiddleware({
+//     serializableCheck: {
+//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//     },
+//   }),
+  
+//   // enhancers: [devToolsEnhancer()],
+
+// })
+
+
+// export default store;
+// make a persisted store and persist the game slice with redux-toolkit
+
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import {combineReducers} from 'redux';
+// import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import gameSlice from '../features/game/gameSlice';
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: AsyncStorage
+}
+
+const rootReducer = combineReducers({
+  game: gameSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store =  configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ['persist/PERSIST'],
+    },
+  }),
   
   // enhancers: [devToolsEnhancer()],
 
 })
+
+
+export default store;
