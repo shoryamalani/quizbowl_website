b641c58fd67574da5382b945b0f7cf8909d2a739
Shorya Malani <shoryamal@gmail.com>
2022-12-28 16:03:44 -0600

    fixed move_dbs

--- quizbowl_backend_server/dbs_worker.py
+++ quizbowl_backend_server/dbs_worker.py
@@ -20,13 +20,12 @@ def createUser():
     # rank
     # user_data
     users = pypika.Table("users")
-    a = pypika.Query.into(users).columns("uuid",'sign_in_count',"last_sign_in","user_token","created_at","questions_attempted","questions_correct","xp","rank","user_data")
-    user_id = str(uuid.uuid4())
+    a = pypika.Query.into(users).columns('sign_in_count',"last_sign_in","user_token","created_at","questions_attempted","questions_correct","xp","rank","user_data")
     token = str(uuid.uuid4())
-    a = a.insert(user_id,0,functions.Now(),token,functions.Now(),0,0,0,0,json.dumps({})) 
+    a = a.insert(0,functions.Now(),token,functions.Now(),0,0,0,0,json.dumps({})) 
     print(a.get_sql())
     execute_db.execute_database_command(conn,a.get_sql())
-    return {"user_id":user_id,"token":token}
+    return {"token":token}
 
 def login(user_id,token):
     conn = get_data_from_database.connect_to_datbase()
